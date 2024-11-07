363f2d00214400a755f9d537076634fbb83e3d26
Aznhelika Khamaza <anzhelikakhamaza@gmail.com>
2024-11-06 20:14:22 -0600

    Add Search Bar functionality. Refactoring

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.onload = fetchCreate();
+const search = document.getElementById("search");
+let postsData = [];
+
 function createElement(tag, className = "", content = "") {
   const element = document.createElement(tag);
   if (className) element.classList.add(className);
@@ -5,57 +9,84 @@ function createElement(tag, className = "", content = "") {
   return element;
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((posts) => {
-    const list = document.querySelector(".post-list");
+function fetchCreate() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      const postList = document.querySelector(".post-list");
+      postsData = posts.map((post) => {
+        const postElement = createPostElement(post);
+        document.querySelector(".post-list").appendChild(postElement);
+        return { ...post, element: postElement };
+      });
+    })
+    .catch((error) => console.error("Error fetching posts:", error));
+}
 
-    posts.forEach((postItem) => {
-      const post = createPostElement(postItem);
-      list.appendChild(post);
-    });
-  })
-  .catch((error) => console.error("Error fetching posts:", error));
-
-function createPostElement(postItem) {
-  const post = createElement("li", "post-list-item");
-  const postTitle = createElement("h4", "post-title", postItem.title);
-  const postText = createElement("p", "post-text", postItem.body);
+function createPostElement(post) {
+  const postItem = createElement("li", "post-list-item");
+  const postTitle = createElement("h4", "post-title", post.title);
+  const postContent = createElement(
+    "p",
+    "post-text",
+    `${post.body.slice(0, 100)}...`
+  );
   const showCommentsButton = createElement(
-    "a",
+    "button",
     "show-comments",
     "Show Comments"
   );
   const hideCommentsButton = createElement(
-    "a",
+    "button",
     "hide-comments",
     "Hide Comments"
+  );
+  const addCommentButton = createElement(
+    "button",
+    "add-new-comment-button",
+    "Add Comment"
   );
 
   hideCommentsButton.style.display = "none";
 
-  post.append(postTitle, postText, showCommentsButton, hideCommentsButton);
+  postItem.append(
+    postTitle,
+    postContent,
+    showCommentsButton,
+    hideCommentsButton,
+    addCommentButton
+  );
 
   const commentSection = createElement("div", "comment-section");
-  post.appendChild(commentSection);
+  postItem.appendChild(commentSection);
 
   showCommentsButton.addEventListener("click", () =>
     showComments(
-      postItem.id,
+      post.id,
       commentSection,
       showCommentsButton,
       hideCommentsButton
     )
   );
+
   hideCommentsButton.addEventListener("click", () =>
     hideComments(commentSection, showCommentsButton, hideCommentsButton)
   );
 
-  return post;
+  addCommentButton.addEventListener("click", () =>
+    createCommentForm(commentSection)
+  );
+
+  return postItem;
 }
 
-function showComments(postId, commentSection, showButton, hideButton) {
-  fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+function showComments(
+  postId,
+  commentSection,
+  showCommentsButton,
+  hideCommentsButton
+) {
+  fetch("https://jsonplaceholder.typicode.com/comments?postId=${postId}")
     .then((response) => response.json())
     .then((comments) => {
       comments.forEach((comment) => {
@@ -63,12 +94,20 @@ function showComments(postId, commentSection, showButton, hideButton) {
         commentSection.appendChild(commentContainer);
       });
 
-      showButton.style.display = "none";
-      hideButton.style.display = "inline-block";
-    });
+      showCommentsButton.style.display = "none";
+      hideCommentsButton.style.display = "inline-block";
+    })
+    .catch((error) => console.error("Error fetching comments:", error));
+}
+
+function hideComments(commentSection, showCommentsButton, hideCommentsButton) {
+  commentSection.innerHTML = "";
+  hideCommentsButton.style.display = "none";
+  showCommentsButton.style.display = "inline-block";
 }
 
 function createCommentElement(comment) {
+  const commentContainer = createElement("div", "comment");
   const commentAuthor = createElement(
     "p",
     "comment-author",
@@ -81,38 +120,67 @@ function createCommentElement(comment) {
   );
   const commentBody = createElement("p", "comment-text", comment.body);
 
-  const addNewComment = createElement("textarea", "add-new-comment");
-  const addNewCommentButton = createElement(
-    "button",
-    "add-new-comment-button",
-    "Add Comment"
-  );
-
-  const commentContainer = createElement("div", "comment");
-  commentContainer.append(
-    commentAuthor,
-    commentEmail,
-    commentBody,
-    addNewComment,
-    addNewCommentButton
-  );
-
-  addNewCommentButton.addEventListener("click", () => {
-    const addedComment = addNewComment.value;
-    const newCommentDisplay = createElement(
-      "p",
-      "new-comment-display",
-      addedComment
-    );
-    commentContainer.append(newCommentDisplay);
-    addNewComment.value = "";
-  });
-
+  commentContainer.append(commentAuthor, commentEmail, commentBody);
   return commentContainer;
 }
 
-function hideComments(commentSection, showButton, hideButton) {
-  commentSection.innerHTML = "";
-  hideButton.style.display = "none";
-  showButton.style.display = "inline-block";
+function createCommentForm(commentSection) {
+  const commentAuthorInput = createElement("textarea", "comment-author-input");
+  const commentEmailInput = createElement("textarea", "comment-email-input");
+  const commentBodyInput = createElement("textarea", "comment-body-input");
+  const submitCommentButton = createElement(
+    "button",
+    "submit-comment-button",
+    "Add Comment"
+  );
+
+  const commentFormContainer = createElement("div", "comment-form-container");
+  commentFormContainer.append(
+    commentAuthorInput,
+    commentEmailInput,
+    commentBodyInput,
+    submitCommentButton
+  );
+  commentSection.prepend(commentFormContainer);
+
+  submitCommentButton.addEventListener("click", () => {
+    const newCommentData = {
+      name: commentAuthorInput.value,
+      email: commentEmailInput.value,
+      body: commentBodyInput.value,
+    };
+
+    const newCommentElement = createCommentElement(newCommentData);
+    commentSection.appendChild(newCommentElement);
+
+    commentAuthorInput.value = "";
+    commentEmailInput.value = "";
+    commentBodyInput.value = "";
+  });
 }
+
+function pushTheComment(commentData) {
+  fetch("https://jsonplaceholder.typicode.com/comments", {
+    method: "POST",
+    body: JSON.stringify(commentData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => console.log(response.json()))
+    .then((data) => {
+      console.log("Comment posted:", data);
+      return data;
+    })
+    .catch((error) => console.error("Error posting comment:", error));
+}
+
+search.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  postsData.forEach((post) => {
+    const isVisible =
+      post.title.toLowerCase().includes(value) ||
+      post.body.toLowerCase().includes(value);
+    post.element.classList.toggle("hide", !isVisible);
+  });
+});
